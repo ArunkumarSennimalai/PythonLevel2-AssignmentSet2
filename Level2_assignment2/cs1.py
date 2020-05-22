2b4506b48fcdeb3ccb52d1e0f711fa66fb782e69
ArunkumarSennimalai <39934063+ArunkumarSennimalai@users.noreply.github.com>
2020-05-22 16:06:27 +0530

    Add files via upload

---
+++ Level2_assignment2/cs1.py
@@ -0,0 +1,44 @@
+import threading
+import sys
+import Queue
+class FileOperations:
+    def fileLinesWordsCount(self,filename):
+        try:
+            with open(filename,"r") as f:
+                linecount=0   
+                wordcount = 0
+                #reading each line
+                for line in f:
+                    #spliting the current line into list of words
+                    words  = line.split()
+                    linecount += 1
+                    wordcount += len(words)
+            #adding the result into the queue
+            q.put([filename,linecount,wordcount])
+        except IOError:
+            print ("%s doesn't exist or error while processing the file\n"%filename)
+
+if __name__ == "__main__":
+    try:
+        #Queue to store the result
+        q = Queue.Queue()
+        #list to store the threads 
+        list1=[]
+        #creating object of class FileOperation to access fileLinesWordsCount method
+        obj1 = FileOperations()
+        #creating thread and appending to list
+        for filename in sys.argv[1:]:
+            list1.append(threading.Thread(target=obj1.fileLinesWordsCount, args=(filename,)))
+        #starting the thread
+        for t in list1:
+            t.start()
+        #invoking join method
+        for t in list1:
+             t.join()
+        #reading queue and printing the result
+        while not q.empty():
+            for i in q.get():
+                print i, "\t",
+            print ("\n")
+    except Exception as e:
+        print (e)
---
+++ Level2_assignment2/cs2.py
@@ -0,0 +1,45 @@
+import os
+import sys
+from glob import glob
+
+class ClassDependencies:
+    def get_subclasses(self,cls):
+        #find subclasses of the class
+        if len(cls.__subclasses__())<=0:
+            return 
+        print cls.__name__ , " [",classes[cls],".py ]"
+        for subclass in cls.__subclasses__():
+            print "\t",subclass.__name__ ," [",classes[subclass],".py ]"
+        print ("\n")
+    def analyse_all_files(self,path):
+        #analyse all files in directory and return dict where key=class value=filename
+        classes = {}
+        filename = path + "*.py"
+        for file in glob(filename):
+            name = os.path.splitext(os.path.basename(file))[0]
+            module = __import__(name)
+            for objname in dir(module):
+                obj = eval("module." + objname)
+                if type(obj) is type:
+                    classes.update({obj:name})
+        return classes
+    def printClassesWithItsSubclasses(self,classes):
+        #iterate dict and call method get_subclasses
+        for obj in classes:
+           self.get_subclasses(obj)
+
+if __name__ == "__main__":
+    try:
+        path = sys.argv[1]
+        sys.path.append(path)
+        obj1 = ClassDependencies()
+        classes = obj1.analyse_all_files(path)
+        obj1.printClassesWithItsSubclasses(classes)
+    except Exception as e:
+        print (e)
+
+
+
+
+
+   
